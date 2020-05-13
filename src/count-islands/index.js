5ad6ff3a9e10d6f0f0baa90ba91e89ba08afb71b
Dexkode <dexkode4@gmail.com>
2020-05-14 00:43:21 +0100

    Count island solved

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,33 @@
-function countIslands(grid) {}
+function countIslands(grid) {
+  let islandCount = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] == 1) {
+        islandCount += 1;
+        dfs(grid, i, j);
+      }
+    }
+  }
+  return islandCount;
+}
+
+let dfs = (grid, i, j) => {
+  if (
+    i < 0 ||
+    i >= grid.length ||
+    j < 0 ||
+    j >= grid[i].length ||
+    grid[i][j] == 0
+  ) {
+    return;
+  }
+  grid[i][j] = 0;
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j - 1);
+  dfs(grid, i, j + 1);
+};
+const grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+console.log(countIslands(grid));
 
 module.exports = countIslands;
