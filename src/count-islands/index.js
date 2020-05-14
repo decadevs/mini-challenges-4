2d78e81999c2a7b165870051a06aa363c96eef64
jack <omudavid@gmail.com>
2020-05-14 10:50:37 +0100

    changes

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,61 @@
-function countIslands(grid) {}
+//Helper function
+function SetToZero(map,a,b) 
+{
+    map[a][b] = 0;
+    if (a + 1 < map.length) 
+    {
+        //land down
+        if (map[a + 1][b] == 1) {
+            SetToZero(map, a + 1, b);
+        }
+    }
+
+    //Land to the right
+    if (map[a][b + 1] == 1) 
+    {
+        SetToZero(map, a, b + 1);
+    }
+
+
+    if(b-1 > 0 )
+    {
+    //land to the left
+    if (map[a][b - 1] == 1)
+    {
+        SetToZero(map,a, b - 1);
+    }
+
+    }
+     
+    //land up
+    if(a-1>0)
+    {
+    if(map[a-1][b] == 1)
+    {
+        SetToZero(map,a-1,b);
+    }
+    }
+}
+
+
+function countIslands(grid) 
+{
+    var counter = 0;
+    for (var i = 0; i < grid.length; i++) 
+    {
+       for (var j = 0; j < grid[i].length; j++) 
+        {
+            if (grid[i][j] == 1)
+            {
+                counter+=1;
+                SetToZero(grid,i,j);
+            }
+            
+        }
+        
+        
+    }
+    return counter;
+}
 
 module.exports = countIslands;
--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,44 @@
-function wordSearch(words, word) {}
+function wordSearch(words, word)
+ { 
+     //Helper function
+     function spellCheck(array,word,a,b)
+     {
+        .unshift
+        //up
+        if(a-1 > 0)
+        {
+            if(a-1 == word[0])
+        }
+     }
 
+
+
+
+     word = word.split("");
+
+     //Looping through the words array index by index
+
+     for (let i = 0; i < words.length; i++) 
+     {
+         for (let j = 0; j < words[i].length; j++) 
+         {
+             if(words[i][j] == word[0])
+             {
+                 spellCheck(words,word,i,j);
+             }
+             
+             
+         }
+         
+     }
+    console.log(word);
+ }
+
+
+wordSearch([
+    ["C", "D", "Y", "C", "X"],
+    ["A", "N", "Y", "Z", "X"],
+    ["T", "F", "Z", "A", "T"],
+    ["M", "D", "B", "U", "T"],
+], "CAT");
 module.exports = wordSearch;
