65709a439dc8b769bc40e38f1e739725dc078a30
seunodewale <seunodewale.zone5@gmail.com>
2020-05-14 22:47:05 +0100

    add solution for count-island

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,31 @@
-function countIslands(grid) {}
+function countIslands(grid) {
+  let numOfIslands = 0;
+  if(grid === null || grid.length === 0){
+    return 0;
+  }
+
+  for(let i=0; i<grid.length; i++){
+    for(let j=0; j<grid[i].length; j++){
+      if(grid[i][j] === 1){
+        numOfIslands += dfs(grid, i, j);
+      }
+    }
+  }
+  return numOfIslands;
+}
+
+function dfs(grid, row, col){
+  if(row<0 || row>=grid.length || col<0 || col>=grid[row].length || grid[row][col] === 0){
+    return 0;
+  }
+
+  grid[row][col] = 0;
+  dfs(grid, row+1, col);
+  dfs(grid, row-1, col);
+  dfs(grid, row, col+1);
+  dfs(grid, row, col-1);
+
+  return 1;
+}
 
 module.exports = countIslands;
--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,16 @@
-function wordSearch(words, word) {}
+function wordSearch(words, word) {
+  let output = '';
+  word.split('').forEach(letter => {
+
+    let i = 0;
+    while(i < words.length){
+      if(words[i][i] === letter){
+        output += words[i][i];
+      }
+      i++;
+    }
+
+  })
+}
 
 module.exports = wordSearch;
