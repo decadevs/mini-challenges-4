320402dfc888f785778d9f681ba1997f55544bbc
adeola idowu <adeolaidowu27@gmail.com>
2020-05-15 09:58:10 +0100

    complete count-islands problem

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,15 @@
-function wordSearch(words, word) {}
+function wordSearch(words, word) {
+  let wordArr = word.split("");
+  const rowLength = words.length;
+  const colLength = words[0].length;
+  for (let row = 0; row < rowLength; row++) {
+    for (let col = 0; col < colLength; col++) {
+      if (words[row][col] === wordArr[0]) {
+        wordArr.shift();
+      }
+    }
+  }
+  return wordArr.length === 0 && true;
+}
 
 module.exports = wordSearch;
