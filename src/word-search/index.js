1b74a4b191799d5cc5ec148c0a9b1d2afddf164d
seunodewale <seunodewale.zone5@gmail.com>
2020-05-15 01:24:28 +0100

    add solution for word-search

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,16 +1,59 @@
 function wordSearch(words, word) {
-  let output = '';
-  word.split('').forEach(letter => {
+  let k = 0;
+  word = word.split('');
 
-    let i = 0;
-    while(i < words.length){
-      if(words[i][i] === letter){
-        output += words[i][i];
+  for (let i=0; i<words.length; i++){
+    for (let j=0; j<words[i].length; j++){
+      if(words[i][j] === word[0]){
+        checkWord(words, word, i, j, k);
       }
-      i++;
+    }
+  }
+
+    if(word.length === 0){
+        return true;
     }
 
-  })
+    return false
+
 }
+
+function checkWord(words, word, row, col, count){
+
+        words[row][col] = 0;
+        count += 1;
+
+        if(row-1 >= 0){
+            if(words[row-1][col] == word[count]){
+                checkWord(words, word, row-1, col, count);
+            }
+        }
+
+        if (col+1 < words[row].length){
+          if (words[row][col + 1] == word[count])
+          {
+              checkWord(words, word, row, col + 1, count);
+          }
+        }
+
+        if(row+1 < words.length){
+          if(words[row+1][col] == word[count]){
+              checkWord(words, word, row+1, col, count);
+          }
+        }
+
+        if(col-1 >= 0){
+            if(words[row][col-1] == word[count]){
+                checkWord(words, word, row, col-1, count);
+            }
+        }
+
+        if(count === word.length){
+            word.splice(0, word.length);
+        }
+
+         words[row][col] = word[count-1];
+
+      }
 
 module.exports = wordSearch;
