07b297644020abb50b5119418238dff2bc9b0816
Thommzy <tomiwaobeisun@yahoo.com>
2020-05-15 03:31:13 +0100

    count islands

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,37 @@
-function countIslands(grid) {}
+function countIslands(grid) {
+  let islands = 0;
+
+  let width = grid.length;
+
+  let height = grid[0].length;
+
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      if (grid[i][j] == "1") {
+        depthFirstSearch(grid, i, j);
+        islands++;
+      }
+    }
+  }
+  return islands;
+
+  function depthFirstSearch(grid, i, j) {
+    let width = grid.length;
+
+    let height = grid[0].length;
+
+    if (i >= 0 && i < width && j >= 0 && j < height && grid[i][j] == "1") {
+      grid[i][j] = "0";
+
+      depthFirstSearch(grid, i + 1, j);
+
+      depthFirstSearch(grid, i, j + 1);
+
+      depthFirstSearch(grid, i - 1, j);
+
+      depthFirstSearch(grid, i, j - 1);
+    }
+  }
+}
 
 module.exports = countIslands;
