af8e371c31e29c5465a27e3a1fdad9e2b0b74aa9
Nwike Ugochukwu <bondesvick@gmail.com>
2020-05-15 21:50:01 +0100

    solved word-search problem

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,23 @@
-function countIslands(grid) {}
+function countIslands(grid) {
+  let count = 0;
+  let flag = 0
+  let colMatch = []
+
+  grid.forEach((element, rootIndex) => {
+    let colIndexes = []
+    element.forEach((item, childIndex) => {
+      if (item == 1 && item !== element[childIndex - 1]) {
+        count++
+        colIndexes.push(childIndex)
+        if (flag > 0 && item === grid[rootIndex - 1][childIndex]) {
+          count--
+        }
+      }
+    })
+    flag++
+    colMatch = colIndexes;
+  });
+  return count;
+}
 
 module.exports = countIslands;
--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,65 @@
-function wordSearch(words, word) {}
+function wordSearch(words, word) {
+  let list = {}
+  let checked = []
+  words.forEach((element, rootI) => {
+    element.forEach((item, childI) => {
+      list[rootI.toString() + childI.toString()] = item
+    })
+  });
+  let wordCopy = word.split()
+  let count = 0
+  match = false
+
+  words.forEach((element, rootI) => {
+    element.forEach((item, childI) => {
+      if (item == word[0] &&
+        list[rootI.toString() + childI.toString()] !== null) {
+        crawler(rootI, childI, 0)
+        if (checked.length == word.length) {
+          match = true
+          return
+        }
+      }
+      //if (match) return
+      checked = []
+    })
+    //if(match) return
+  });
+
+  function crawler(row, col, matchIndex) {
+    count++
+    checked.push(row.toString() + col.toString())
+    list[row.toString() + col.toString()] = null
+    if (checked.length == word.length) {
+      return true
+    }
+
+    if ((row > 0) &&
+      words[row - 1][col] == word[matchIndex + 1] &&
+      list[(row - 1).toString() + col.toString()] !== null) {
+      //count++
+      crawler(row - 1, col, matchIndex + 1)
+      //return
+    } else if ((row < words.length - 1) &&
+      words[row + 1][col] == word[matchIndex + 1] &&
+      list[(row + 1).toString() + col.toString()] !== null) {
+
+      crawler(row + 1, col, matchIndex + 1)
+
+    } else if (words[row][col - 1] == word[matchIndex + 1] &&
+      list[row.toString() + (col - 1).toString()] !== null) {
+
+      crawler(row, col - 1, matchIndex + 1)
+
+    } else if (words[row][col + 1] == word[matchIndex + 1] &&
+      list[row.toString() + (col + 1).toString()] !== null) {
+
+      crawler(row, col + 1, matchIndex + 1)
+
+    }
+  }
+
+  return match
+}
 
 module.exports = wordSearch;
