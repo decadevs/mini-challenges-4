ce52ad7bfae013cd501db33d8919e536a4b310e5
SlimzyCM <chidimicheal17@gmail.com>
2020-05-15 16:47:02 +0100

    Solved the word-search algorithm

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,29 @@
-function wordSearch(words, word) {}
+function wordSearch(words, word) {
+  const rows = words.length;
+  const cols = words[0].length;
+
+  let n = 0;
+  let check = false;
+
+  for (let i = 0; i < words.length; i++) {
+      for (let j = 0; j < words[i].length; j++) {
+          if(words[i][j] == word[0])
+              if(search(i,j,n)) check = true;
+      }
+  }
+
+  function search (i, j, n) {
+      if(n === word.length) return true;
+      if(i >= 0 && i < rows && j >= 0 && j < cols && words[i][j] === word[n]){
+          words[i][j] = '$';
+          const searchFound = search(i-1,j,n+1) || search(i+1,j,n+1) || search(i,j-1,n+1) || search(i,j+1,n+1)
+          words[i][j] = word[n];
+          return searchFound;
+      }
+      return false
+  }
+  return check;
+
+}
 
 module.exports = wordSearch;
