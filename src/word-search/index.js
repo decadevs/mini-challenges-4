829f1551baaaa0b77c17089e7c341095aa84e473
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-05-15 22:59:07 +0100

    steps to approach word search

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,15 @@
-function wordSearch(words, word) {}
+/*Steps to word search
+
+1. get index positions of each letter of word from words
+2. while visited not empty
+a. for each of the row,col pair, compute the adjacents left, adjacent bottom from the pair,
+b. updated visited with adjacent positon pair if is same as index position pair of word
+c. get alphabet representations of visited pairs from words
+d. if all word is in alpha reprensation- then the word was found
+*/
+
+function wordSearch(words, word) {
+
+}
 
 module.exports = wordSearch;
