4a880f0e320c99a07de0e2aedd5fe5b6b5ebde1c
engineer-kufre <engineer_kufre@yahoo.com>
2020-05-15 19:07:52 +0100

    new tests added and algorithm modification to solve them

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,35 +1,42 @@
 function wordSearch(words, word) {
-  let queue = [];
+  let stack = [];
   for (let row = 0; row < words.length; row++) {
     for (let col = 0; col < words[0].length; col++) {
       if (words[row][col] === word[0]) {
-        queue.push([row, col]);
+        stack.push([row, col]);
         let comparisonString = word[0];
-        let wordIndex = 0;
+        let wordIndex = 1;
         let visited = [row.toString() + col.toString()];
-        while (queue.length && wordIndex < word.length) {
-          let [nodeX, nodeY] = queue.shift();
-          wordIndex++;
+        while (stack.length && wordIndex !== word.length) {
+          let [nodeX, nodeY] = stack[stack.length - 1];
           if (nodeX - 1 >= 0 && words[nodeX - 1][nodeY] === word[wordIndex] && !visited.includes((nodeX - 1).toString() + nodeY.toString())) {
-            queue.push([nodeX - 1, nodeY]);
+            stack.push([nodeX - 1, nodeY]);
             visited.push((nodeX - 1).toString() + nodeY.toString());
             comparisonString += word[wordIndex];
+            wordIndex++;
           }
           if (nodeY - 1 >= 0 && words[nodeX][nodeY - 1] === word[wordIndex] && !visited.includes(nodeX.toString() + (nodeY - 1).toString())) {
-            queue.push([nodeX, nodeY - 1]);
+            stack.push([nodeX, nodeY - 1]);
             visited.push(nodeX.toString() + (nodeY - 1).toString());
             comparisonString += word[wordIndex];
+            wordIndex++;
           }
-          if (nodeX + 1 < words.length && words[nodeX + 1][nodeY] === word[wordIndex] && !visited.includes((nodeX + 1).toString() + nodeY.toString())) {
-            queue.push([nodeX + 1, nodeY]);
+          else if (nodeX + 1 < words.length && words[nodeX + 1][nodeY] === word[wordIndex] && !visited.includes((nodeX + 1).toString() + nodeY.toString())) {
+            stack.push([nodeX + 1, nodeY]);
             visited.push((nodeX + 1).toString() + nodeY.toString());
             comparisonString += word[wordIndex];
+            wordIndex++;
           }
-          if (nodeY + 1 < words[0].length && words[nodeX][nodeY + 1] === word[wordIndex] && !visited.includes(nodeX.toString() + (nodeY + 1).toString())) {
-            queue.push([nodeX, nodeY + 1]);
+          else if (nodeY + 1 < words[0].length && words[nodeX][nodeY + 1] === word[wordIndex] && !visited.includes(nodeX.toString() + (nodeY + 1).toString())) {
+            stack.push([nodeX, nodeY + 1]);
             visited.push(nodeX.toString() + (nodeY + 1).toString());
             comparisonString += word[wordIndex];
+            wordIndex++;
           }
+          else {
+            stack.pop();
+          }
+
         }
         if (comparisonString === word) {
           return true;
