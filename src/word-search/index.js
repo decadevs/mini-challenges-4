fb837068a38611a5f66eaa509eb942f58b99c456
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-05-16 06:05:53 +0100

    check if first and last word occurence appears once in words

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -18,74 +18,98 @@ d. if all word is in alpha reprensation- then the word was found
 // const word="SANCTIONS"
 
 function wordSearch(words, word) {
-
-    let wordIndex = [];
-    for (let p = 0; p < words.length; p++) {
-      for (m = 0; m < words[p].length; m++) {
-        if (word.includes(words[p][m])) {
-          wordIndex.push([p, m]);
-        }
+  let wordIndex = [];
+  for (let p = 0; p < words.length; p++) {
+    for (m = 0; m < words[p].length; m++) {
+      if (word.includes(words[p][m])) {
+        wordIndex.push([p, m]);
       }
     }
-    if(word[0]===word[word.length-1]){
-        return false
-    }
+  }
 
-    let i =0
-    let visited=[]
-    visited.push(wordIndex[0]) //initialize visited with first index array
-
-    while(i<visited.length){
-        let row_index=visited[i][0]
-        let col_index=visited[i][1]
-
-        let right_ajacent_index=[row_index,col_index+1]
-        let bottom_ajacent_index=[row_index+1,col_index]
-        let left_ajacent_index=[row_index,col_index-1]
-        let top_ajacent_index=[row_index-1,col_index]
-
-        for(let x=0;x<wordIndex.length;x++){
-            if(JSON.stringify(wordIndex[x])===JSON.stringify(right_ajacent_index) && (visited.every(e=>JSON.stringify(e)!==JSON.stringify(right_ajacent_index)))){
-                visited.push(right_ajacent_index)
-            }
-            if (JSON.stringify(wordIndex[x]) === JSON.stringify(bottom_ajacent_index) && visited.every((e) => JSON.stringify(e) !== JSON.stringify(bottom_ajacent_index))) {
-              visited.push(bottom_ajacent_index);
-            }
-            if (JSON.stringify(wordIndex[x]) == JSON.stringify(left_ajacent_index) && visited.every((e) => JSON.stringify(e) !== JSON.stringify(left_ajacent_index))) {
-              visited.push(left_ajacent_index);
-            }
-            if (JSON.stringify(wordIndex[x]) === JSON.stringify(top_ajacent_index) && visited.every((e) => JSON.stringify(e) !== JSON.stringify(top_ajacent_index))) {
-              visited.push(top_ajacent_index);
-            }
+  
+  if (word[0] === word[word.length - 1]) {
+      //check if the word appears only once
+      let count = words.reduce(function (acc, curr) {
+        if (curr.includes(word[0])) {
+          acc += 1;
         }
+        return acc;
+      }, 0);
+      if(count<2){
+        return false;
+      }
+  }
 
-        i+=1
+  let i = 0;
+  let visited = [];
+  visited.push(wordIndex[0]); //initialize visited with first index array
 
+  while (i < visited.length) {
+    let row_index = visited[i][0];
+    let col_index = visited[i][1];
+
+    let right_ajacent_index = [row_index, col_index + 1];
+    let bottom_ajacent_index = [row_index + 1, col_index];
+    let left_ajacent_index = [row_index, col_index - 1];
+    let top_ajacent_index = [row_index - 1, col_index];
+
+    for (let x = 0; x < wordIndex.length; x++) {
+      if (
+        JSON.stringify(wordIndex[x]) === JSON.stringify(right_ajacent_index) &&
+        visited.every(
+          (e) => JSON.stringify(e) !== JSON.stringify(right_ajacent_index)
+        )
+      ) {
+        visited.push(right_ajacent_index);
+      }
+      if (
+        JSON.stringify(wordIndex[x]) === JSON.stringify(bottom_ajacent_index) &&
+        visited.every(
+          (e) => JSON.stringify(e) !== JSON.stringify(bottom_ajacent_index)
+        )
+      ) {
+        visited.push(bottom_ajacent_index);
+      }
+      if (
+        JSON.stringify(wordIndex[x]) == JSON.stringify(left_ajacent_index) &&
+        visited.every(
+          (e) => JSON.stringify(e) !== JSON.stringify(left_ajacent_index)
+        )
+      ) {
+        visited.push(left_ajacent_index);
+      }
+      if (
+        JSON.stringify(wordIndex[x]) === JSON.stringify(top_ajacent_index) &&
+        visited.every(
+          (e) => JSON.stringify(e) !== JSON.stringify(top_ajacent_index)
+        )
+      ) {
+        visited.push(top_ajacent_index);
+      }
     }
 
+    i += 1;
+  }
 
-    let alphabet_pos_visited=[]
+  let alphabet_pos_visited = [];
 
-    for(let j=0;j<visited.length;j++){
-        alphabet_pos_visited.push(words[visited[j][0]][visited[j][1]])
+  for (let j = 0; j < visited.length; j++) {
+    alphabet_pos_visited.push(words[visited[j][0]][visited[j][1]]);
+  }
 
+  let count = 0;
+  for (let w = 0; w < word.length; w++) {
+    if (alphabet_pos_visited.includes(word[w])) {
+      count += 1;
     }
-    
+  }
 
-    let count=0
-    for(let w=0;w<word.length;w++){
-        if(alphabet_pos_visited.includes(word[w])){
-            count+=1
-        }
-    }
-    
-
-    if(count===word.length){
-        return true
-    }
-    else{
-        return false
-    }
+  if (count === word.length) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = wordSearch;
