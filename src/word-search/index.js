09516cf9724c6d6bdb81615528b914d6f5bb995e
Nwike Ugochukwu <bondesvick@gmail.com>
2020-05-16 09:13:42 +0100

    pulled changes from upstream and updated my solution

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,57 +1,66 @@
 function wordSearch(words, word) {
   let list = {}
   let checked = []
-  words.forEach((element, rootI) => {
-    element.forEach((item, childI) => {
-      list[rootI.toString() + childI.toString()] = item
-    })
-  });
-  let wordCopy = word.split()
-  let count = 0
+
+  function Flag() {
+    words.forEach((element, rootI) => {
+      element.forEach((item, childI) => {
+        list[rootI.toString() + childI.toString()] = item
+      })
+    });
+  }
+
+  let wordCopy
   match = false
 
   words.forEach((element, rootI) => {
     element.forEach((item, childI) => {
-      if (item == word[0] &&
-        list[rootI.toString() + childI.toString()] !== null) {
+      Flag()
+      checked = []
+      wordCopy = word.split('')
+      if (item == word[0]) {
+
         crawler(rootI, childI, 0)
-        if (checked.length == word.length) {
+
+        if (checked.join('') == word) {
           match = true
           return
         }
+
       }
-      //if (match) return
-      checked = []
     })
-    //if(match) return
   });
 
   function crawler(row, col, matchIndex) {
-    count++
-    checked.push(row.toString() + col.toString())
-    list[row.toString() + col.toString()] = null
-    if (checked.length == word.length) {
-      return true
+   if (wordCopy.length == 0) {
+     return
+   }
+    if (word[matchIndex] == wordCopy[0]) {
+      checked.push(wordCopy.shift())
     }
+    list[row.toString() + col.toString()] = null
 
     if ((row > 0) &&
       words[row - 1][col] == word[matchIndex + 1] &&
       list[(row - 1).toString() + col.toString()] !== null) {
-      //count++
+
       crawler(row - 1, col, matchIndex + 1)
-      //return
-    } else if ((row < words.length - 1) &&
+
+    }
+    if ((row < words.length - 1) &&
       words[row + 1][col] == word[matchIndex + 1] &&
       list[(row + 1).toString() + col.toString()] !== null) {
 
       crawler(row + 1, col, matchIndex + 1)
 
-    } else if (words[row][col - 1] == word[matchIndex + 1] &&
+    }
+    if (words[row][col - 1] == word[matchIndex + 1] &&
       list[row.toString() + (col - 1).toString()] !== null) {
 
       crawler(row, col - 1, matchIndex + 1)
 
-    } else if (words[row][col + 1] == word[matchIndex + 1] &&
+    }
+    if (words[row][col + 1] == word[matchIndex + 1] &&
       list[row.toString() + (col + 1).toString()] !== null) {
 
       crawler(row, col + 1, matchIndex + 1)
