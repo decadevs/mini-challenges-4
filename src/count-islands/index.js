3b935bda45f1bbe08310eed741f332e7186cc39d
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-05-17 03:04:00 +0100

    highlight summary step to island count

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,11 @@
-function countIslands(grid) {}
+/*steps to count island
+1. count island as you encounter unvisited land- 1
+2. making sure to mark as visited every other 1's adjacent to the unvisited land recursively
+3. return island count as number of island in the grid
+*/
+
+function countIslands(grid) {
+
+}
 
 module.exports = countIslands;
