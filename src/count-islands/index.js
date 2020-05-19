ac7cd28f9d10cdff4de7ec72e26fc2e4c74455e4
Nwike Ugochukwu <bondesvick@gmail.com>
2020-05-19 16:48:49 +0100

    optimized my solution for better perfomance

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,29 @@
+function crawler(row, col, grid, list) {
+  list[row.toString() + col.toString()] = null
+
+  if ((row > 0) &&
+    grid[row - 1][col] == 1 &&
+    list[(row - 1).toString() + col.toString()] !== null) {
+    crawler(row - 1, col, grid, list)
+
+  }
+  if ((row < grid.length - 1) &&
+    grid[row + 1][col] == 1 &&
+    list[(row + 1).toString() + col.toString()] !== null) {
+    crawler(row + 1, col, grid, list)
+
+  }
+  if (grid[row][col - 1] == 1 &&
+    list[row.toString() + (col - 1).toString()] !== null) {
+    crawler(row, col - 1, grid, list)
+
+  }
+  if (grid[row][col + 1] == 1 &&
+    list[row.toString() + (col + 1).toString()] !== null) {
+    crawler(row, col + 1, grid, list)
+  }
+}
+
 function countIslands(grid) {
   let count = 0
   let list = {}
@@ -12,34 +38,12 @@ function countIslands(grid) {
     element.forEach((item, childIndex) => {
       if (item == 1 &&
         list[rootIndex.toString() + childIndex.toString()] !== null) {
+        
         count++
-        crawler(rootIndex,childIndex)
+        crawler(rootIndex, childIndex, grid, list)
       }
     })
   });
-
-  function crawler(row, col) {
-    list[row.toString() + col.toString()] = null
-
-    if ((row > 0) &&
-      grid[row - 1][col] == 1 &&
-      list[(row - 1).toString() + col.toString()] !== null) {
-      crawler(row - 1, col)
-
-      } if ((row < grid.length - 1) &&
-        grid[row + 1][col] == 1 &&
-        list[(row + 1).toString() + col.toString()] !== null) {
-      crawler(row + 1, col)
-
-      } if (grid[row][col - 1] == 1 &&
-        list[row.toString() + (col - 1).toString()] !== null) {
-      crawler(row, col - 1)
-
-      } if (grid[row][col + 1] == 1 &&
-        list[row.toString() + (col + 1).toString()] !== null) {
-          crawler(row, col + 1)
-      }
-  }
 
   return count;
 }
--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,19 +1,73 @@
-function wordSearch(words, word) {
 
+//#region - A function that creates key-value-pairs of all the letters in the array
+// and store them in list object
+function Flag(words, list) {
+  words.forEach((element, rootI) => {
+    element.forEach((item, childI) => {
+      list[rootI.toString() + childI.toString()] = item
+    })
+  });
+}
+//#endregion
+
+//#region - A dfs function that handles searching adjacent characters
+// that matches with the next letter in the word
+function crawler(row, col, matchIndex, checked, wordCopy, word, words, list) {
+  if (wordCopy.length == 0) {
+    return
+  }
+  if (word[matchIndex] == wordCopy[0]) {
+    checked.push(wordCopy.shift())
+  }
+
+  //Set already matched letter to null in the list object
+  list[row.toString() + col.toString()] = null
+
+  //#region - All the if statements whithin this region simply checks all the adjacent letters
+  // If there is a match, the dfs funtion is called recursively; passing the mathching
+  // adjacent index values to it
+  if ((row > 0) &&
+    words[row - 1][col] == word[matchIndex + 1] &&
+    list[(row - 1).toString() + col.toString()] !== null) {
+
+    crawler(row - 1, col, matchIndex + 1, checked, wordCopy, word, words, list)
+
+  }
+  if ((row < words.length - 1) &&
+    words[row + 1][col] == word[matchIndex + 1] &&
+    list[(row + 1).toString() + col.toString()] !== null) {
+
+    crawler(row + 1, col, matchIndex + 1, checked, wordCopy, word, words, list)
+
+  }
+  if (words[row][col - 1] == word[matchIndex + 1] &&
+    list[row.toString() + (col - 1).toString()] !== null) {
+
+    crawler(row, col - 1, matchIndex + 1, checked, wordCopy, word, words, list)
+
+  }
+  if (words[row][col + 1] == word[matchIndex + 1] &&
+    list[row.toString() + (col + 1).toString()] !== null) {
+
+    crawler(row, col + 1, matchIndex + 1, checked, wordCopy, word, words, list)
+
+  }
+  //#endregion
+
+  // Check if already matched word is the same as the given word
+  // if false we pop off the last matched letter and return
+  if (checked.join('') !== word) {
+    wordCopy.unshift(checked.pop())
+    list[row.toString() + col.toString()] = wordCopy[0]
+    return
+  }
+}
+//#endregion
+
+function wordSearch(words, word) {
   // A Object that holds all the letters in the given words array
   let list = {}
   let checked = []
-
-  //#region - A function that creates key-value-pairs of all the letters in the array
-  // and store them in list object
-  function Flag() {
-    words.forEach((element, rootI) => {
-      element.forEach((item, childI) => {
-        list[rootI.toString() + childI.toString()] = item
-      })
-    });
-  }
-  //#endregion
 
   let wordCopy
   match = false
@@ -23,7 +77,7 @@ function wordSearch(words, word) {
     element.forEach((item, childI) => {
 
       //calling the fuction for each iteration to remove null values in the list object
-      Flag()
+      Flag(words, list)
 
       // an array that collects serial word mathes
       checked = []
@@ -33,7 +87,7 @@ function wordSearch(words, word) {
       if (item == word[0]) {
 
         // calling the dfs function
-        crawler(rootI, childI, 0)
+        crawler(rootI, childI, 0, checked, wordCopy, word, words, list)
 
         if (checked.join('') == word) {
           match = true
@@ -42,58 +96,6 @@ function wordSearch(words, word) {
       }
     })
   });
-  //#endregion
-
-  //#region - A dfs function that handles searching adjacent characters that matches with the next letter in the word
-  function crawler(row, col, matchIndex) {
-   if (wordCopy.length == 0) {
-     return
-   }
-    if (word[matchIndex] == wordCopy[0]) {
-      checked.push(wordCopy.shift())
-    }
-
-    //Set already matched letter to null in the list object
-    list[row.toString() + col.toString()] = null
-
-    //#region - All the if statements whithin this region simply checks all the adjacent letters
-    // If there is a match, the dfs funtion is called recursively; passing the mathching
-    // adjacent index values to it
-    if ((row > 0) &&
-      words[row - 1][col] == word[matchIndex + 1] &&
-      list[(row - 1).toString() + col.toString()] !== null) {
-
-      crawler(row - 1, col, matchIndex + 1)
-
-    }
-    if ((row < words.length - 1) &&
-      words[row + 1][col] == word[matchIndex + 1] &&
-      list[(row + 1).toString() + col.toString()] !== null) {
-
-      crawler(row + 1, col, matchIndex + 1)
-
-    }
-    if (words[row][col - 1] == word[matchIndex + 1] &&
-      list[row.toString() + (col - 1).toString()] !== null) {
-
-      crawler(row, col - 1, matchIndex + 1)
-
-    }
-    if (words[row][col + 1] == word[matchIndex + 1] &&
-      list[row.toString() + (col + 1).toString()] !== null) {
-
-      crawler(row, col + 1, matchIndex + 1)
-
-    }
-    //#endregion
-
-    // Check if already matched word is the same as the given word
-    // if false we pop off the last matched letter and return
-    if (checked.join('') !== word) {
-      wordCopy.unshift(checked.pop())
-      return
-    }
-  }
   //#endregion
 
   return match
