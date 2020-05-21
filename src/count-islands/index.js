826ed054cbe624d8b775d39318e49921d2a25aa5
Oche Ejeh <35895581+goodnessemmanuel@users.noreply.github.com>
2020-05-21 03:47:47 +0100

    Attempted Mini Challenge 4
    @flinchy I just made the modifications as instructed:
    
    - moved out the searchIsland function
    - updated the test cases
    
    Apologies for the lateness. Terrible power supply, I was also trying to improve my code functionality, among others.
    
    Thank you, sir.

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,49 +1,18 @@
 function countIslands(grid) 
 {
-    //check to ensure a valid array was entered
-    if (!Array.isArray(grid))
+    //check to ensure a valid array was entered and it must not be empty
+    if (!Array.isArray(grid) || grid.length === 0)
     {
         return;
-    }
-
-    //function variable to keep track of neigbouring lands(1's) with respect to the current land(1)
-    let searchIslands = function (islandArray, row, col, track)
-    {
-        //perform the underly check to ensure the operation is valid
-        if (row < 0 || row > islandArray.length - 1 || col < 0 || col > islandArray[row].length - 1)
-        {
-            return false;
-        }
-        
-        //if the index has been tracked before, dont continue
-        if (track[row][col])
-        {
-            return false;
-        }
-        //if index has not been tracked, set the value to true now that it is on track
-        track[row][col] = true; 
-        
-        //if the item been tracked is not a land, discontinue the operation
-        if (islandArray[row][col] !== 1)
-        {
-            return false;
-        }
-
-        //tranverse the adjacent elements to the element with the current index
-        //recursively via vertical and horizontal direction. 
-        searchIslands(islandArray, row - 1, col, track);
-        searchIslands(islandArray, row + 1, col, track);
-        searchIslands(islandArray, row, col - 1, track);
-        searchIslands(islandArray, row, col + 1, track);
     }
 
     let countOfIslands = 0;
     let gridLength = grid.length;
     //initialize boolean array with thesame dimension as the input array and pre-populate 
     //it with false values to keep track of all lands pending when it will be visited 
-    let keepTrack = new Array(gridLength).fill([]).map((item, index) => 
-                    new Array(grid[index]).fill(false));
+    let keepTrack = new Array(gridLength).fill([]).map((item, index) => new Array(grid[index]).fill(false));
     
+    //loop through the inputed grid(i.e arrays of 1's & 0's) and perform the below operation.
     for (let i = 0; i < grid.length; i++)
     {
         for (let j = 0; j < grid[i].length; j++)
@@ -61,4 +30,34 @@ function countIslands(grid)
     return countOfIslands;
 }
 
+//function declaration with mechanism to count islands without repeatition 
+function searchIslands (islandArray, row, col, track)
+{
+    //perform the underly check to ensure the operation is valid
+    if (row < 0 || row > islandArray.length - 1 || col < 0 || col > islandArray[row].length - 1)
+    {
+        return false;
+    }
+        
+    //if the index has been tracked before, dont continue
+    if (track[row][col])
+    {
+        return false;
+    }
+    //if index has not been tracked, set the value to true now that it is on track
+    track[row][col] = true; 
+        
+    //if the item been tracked is not a land, discontinue the operation
+    if (islandArray[row][col] !== 1)
+    {
+        return false;
+    }
+
+    //tranverse the adjacent elements to the element with the current index
+    //recursively via vertical and horizontal direction. 
+    searchIslands(islandArray, row - 1, col, track);
+    searchIslands(islandArray, row + 1, col, track);
+    searchIslands(islandArray, row, col - 1, track);
+    searchIslands(islandArray, row, col + 1, track);
+}
 module.exports = countIslands;
--- src/word-search/index.js
+++ src/word-search/index.js
@@ -5,40 +5,10 @@ function wordSearch(words, word)
     {
         return `Please enter a valid input`;
     }
-    
-    //definition of a function variable to handle transversing during search
-    const searchMechanism  = 
-    function (words, row, col, wordToSearch, indexToSearch, searchTrack, container)
-    {
-        //discontinue the search(i.e transverse) if any of this constraints occur
-        if (row < 0  || row > words.length - 1 || col < 0 || col > words[row].length - 1)
-        {
-            return;
-        }
-        //discontinue the current search(i.e transverse) if any letter of the target  
-        //word is not found in the words or such letter has already been searched
-        if (searchTrack[row][col] || words[row][col] !== wordToSearch[indexToSearch])
-        {
-            return;
-        }
-
-        searchTrack[row][col] = true;//keep record of all search made in the boolean array        
-        container.push(words[row][col]);//store the search result in the container array
-        indexToSearch++ //increment the search index for the next letter inline
-
-        //recursively target all the adjecent element during search vertically and horizontally 
-        searchMechanism(words, row - 1, col, wordToSearch, indexToSearch, searchTrack, container);
-        searchMechanism(words, row + 1, col, wordToSearch, indexToSearch, searchTrack, container);
-        searchMechanism(words, row, col + 1, wordToSearch, indexToSearch, searchTrack, container);
-        searchMechanism(words, row, col - 1, wordToSearch, indexToSearch, searchTrack, container);
-    }
-
-    let holdWord = []; //initialization of variable array as container to hold searched word;
-    let indexToSearch = 0;
-
-    //initialization of a 2D boolean array with default values set to false
-    //and thesame dimension as words to keep track of search made 
-    let searchTrack = new Array(words.length).fill([]).map((e, i) => new Array(words[i].length).fill(false));
+    //declaration of variables
+    let holdWord = []; //letters found would be stored here
+    let indexToSearch = 0;//first index of the target word letter before subsequent letters   
+    let searchTrack = [];//2D boolean array to keep track of each search made
 
     //loop through all the items of words array and tranverse through them while searching for the target word
     for (let i = 0; i < words.length; i++)
@@ -47,13 +17,45 @@ function wordSearch(words, word)
         {
             if (words[i][j] === word[indexToSearch] && holdWord.join('') !== word)
             {
-                holdWord = [];//refresh the container at the start of each new search transverse 
+                holdWord = [];//refresh the container at the start of each new search transverse
+                //set all the already decalared boolean variable values to false at each transverse  
+                searchTrack = new Array(words.length).fill([]).map((e, i) => new Array(words[i].length).fill(false));
                 searchMechanism(words, i, j, word, indexToSearch, searchTrack, holdWord);
             }
         }
     }
     //if you find the word return true, if not, return false.
     return holdWord.join('') === word? true : false;
+}
+
+//declaration of a function to handle transversing during search
+function searchMechanism (words, row, col, wordToSearch, indexToSearch, searchTrack, container)
+{
+    //discontinue the search(i.e transverse) if any of this constraints is met
+    if (row < 0  || row > words.length - 1 || col < 0 || col > words[row].length - 1)
+    {
+        return;
+    }
+    //discontinue the current search(i.e transverse) if any letter of the target  
+    //word is not found in the words or such letter has already been searched
+    if (searchTrack[row][col] || words[row][col] !== wordToSearch[indexToSearch])
+    {
+        return;
+    }
+
+    searchTrack[row][col] = true;//keep record of all search made in the boolean array        
+    container.push(words[row][col]);//store the search result in the container array
+    indexToSearch++ //increment the search index for the next letter inline
+    
+    if (indexToSearch < container.length)//ensure adjacent duplicate letters are not repeated...
+    {
+        container.pop();//...if any letter is added more than once, remove it.
+    }
+    //recursively target all the adjecent element during search vertically and horizontally using the underly function
+    searchMechanism(words, row - 1, col, wordToSearch, indexToSearch, searchTrack, container);
+    searchMechanism(words, row + 1, col, wordToSearch, indexToSearch, searchTrack, container);
+    searchMechanism(words, row, col + 1, wordToSearch, indexToSearch, searchTrack, container);
+    searchMechanism(words, row, col - 1, wordToSearch, indexToSearch, searchTrack, container);
 }
 
 module.exports = wordSearch;
