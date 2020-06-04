dee2fcee10cf08652b40e089b9bd9df0dc73b937
warami13 <wrmyeresanara@gmail.com>
2020-06-04 12:29:42 +0100

    add count-island solution

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,50 @@
-function countIslands(grid) {}
+/*
+- use recursion method (method that calls itself)
+- use depth first search
+
+- create a variable to hold the count
+- figure out the edge cases
+  - if there are no islands then return 0;
+- the first loop i will be used to keep track of the outer loop i.e. the rows
+- the second loop j will be used to keep track of the inner loop i.e. the columns
+- if the grid at any point has 1 it should increase the count;
+
+- figure out how to keep track of the island
+  - find an island we change it to 0 to mark as done? i.e. modifying
+*/
+
+function countIslands(grid) {
+    let islandCount = 0;
+    let rows = grid.length
+    let columns = grid[0].length
+
+    if (rows === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            if (grid[i][j] === 1) {
+                checkIsland(i, j, grid)
+                islandCount++;
+                console.log(islandCount);
+
+            }
+        }
+    }
+    return islandCount;
+
+    function checkIsland(i, j, grid) {
+        if ((i < 0 || j < 0) || (i >= rows || j >= columns) || grid[i][j] !== 1) {
+            return;
+        }
+        grid[i][j] = 0;
+
+        checkIsland(i + 1, j, grid)
+        checkIsland(i - 1, j, grid)
+        checkIsland(i, j - 1, grid)
+        checkIsland(i, j + 1, grid)
+    }
+};
 
 module.exports = countIslands;
--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,66 @@
-function wordSearch(words, word) {}
+/*
+- use recursion method (method that calls itself)
+- use depth first search
+
+
+- if there are no words return false;
+- the first loop i will be used to keep track of the outer loop i.e. the rows
+- the second loop j will be used to keep track of the inner loop i.e. the columns
+- if the grid at any point has the letters it should push to the variable;
+
+- figure out how to keep track of the word search
+  - modify?
+*/
+
+function wordSearch(words, word) {
+    let rows = words.length
+    let columns = words[0].length
+    if (word === '') return false;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            if (words[i][j] === word[i]) {
+                console.log(word[i]);
+                if (wordCheck(i, j)) return true;
+            }
+        }
+    }
+    return false;
+
+    function wordCheck(i, j) {
+        console.log(i, j);
+
+        if ((i === word.length)) return true;
+        if (!words[i] || !words[i][j]) return false;
+
+
+        if (wordCheck(i + 1, j)) return true;
+        if (wordCheck(i - 1, j)) return true;
+        if (wordCheck(i, j - 1)) return true;
+        if (wordCheck(i, j + 1)) return true;
+
+        return false;
+    }
+}
+
+// words = [
+//     ["C", "D", "Y", "C", "X"],
+//     ["A", "N", "Y", "Z", "X"],
+//     ["T", "F", "Z", "A", "T"],
+//     ["M", "D", "B", "U", "T"],
+// ];
+
+// word = "CAT";
+
+words = [
+    ["D", "S", "A", "N", "C"],
+    ["I", "N", "O", "I", "T"],
+    ["T", "A", "T", "R", "O"],
+    ["M", "F", "O", "U", "T"],
+];
+
+word = "SANCTIONS";
+console.log(wordSearch(words, word));
+
 
 module.exports = wordSearch;
