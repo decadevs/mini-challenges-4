dbd0fd02acb2b693616d7af29e6a374ddd717b0b
Anabiz <anabizconcept9@gmail.com>
2020-06-15 23:06:09 +0100

    possible solution to count island challenge

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,40 @@
-function countIslands(grid) {}
+function countIslands(grid) {
+  if (grid.length === 0 || grid === null) {
+    return 0
+  };
+
+  let islandCounter = 0;
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+
+      if (grid[row][col] === 1) {
+        islandCounter++;
+
+        checkingForIsland(grid, row, col)
+      }
+    }
+  };
+  return islandCounter
+  };
+
+
+  function checkingForIsland(grid, row, col) {
+
+    if (grid[row] === undefined || grid[row][col] === undefined || grid[row][col] === 0) {
+      return 0;
+    };
+
+    grid[row][col] = "#";
+
+    checkingForIsland(grid, row + 1, col)
+    checkingForIsland(grid, row - 1, col)
+    checkingForIsland(grid, row, col + 1)
+    checkingForIsland(grid, row, col - 1)
+
+    return;
+}
 
 module.exports = countIslands;
+
+
