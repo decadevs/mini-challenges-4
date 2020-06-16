a44857f08a5184dc0147ef967d0c1780b7163b33
Anabiz <anabizconcept9@gmail.com>
2020-06-16 09:14:39 +0100

    possible solution to word search challenge

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,42 @@
-function wordSearch(words, word) {}
+function wordSearch(board, word) {
 
+  for(let row=0; row<board.length;row++){
+      for(let col=0; col<board[row].length; col++){
+          if (board[row][col]===word[0]){
+             if(checkNext(board, word, row, col, 0, "",0)){
+                 return true;
+             }
+
+          }
+      }
+  }
+  return false
+}
+  function checkNext(board, word, row, col, wordCharIndex, wordComparator){
+
+      if(wordComparator == word){
+        return 1;
+      }
+      if((row<0||row>=board.length||col<0||col>=board[row].length||board[row][col] !== word[wordCharIndex])){
+          return 0;
+      }
+      wordComparator +=board[row][col];
+      board[row][col]=0;
+
+      if(checkNext(board,word, row-1, col, wordCharIndex+1, wordComparator)){
+        return true;
+      }
+      if(checkNext(board,word, row+1, col, wordCharIndex+1, wordComparator)){
+        return true;
+      }
+      if(checkNext(board,word, row, col-1, wordCharIndex+1, wordComparator)){
+        return true;
+      }
+      if(checkNext(board,word, row, col+1, wordCharIndex+1, wordComparator)){
+        return true;
+      }
+
+  }
 module.exports = wordSearch;
+
+
