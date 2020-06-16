a5e0bf2323d59e65f1be964f483178d8c4f6e7a8
faith okpala <okpalafaith1@gmail.com>
2020-06-16 18:44:59 +0100

    word search

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,27 @@
-function countIslands(grid) {}
+function countIslands(grid) {
+
+    let count = 0;
+
+
+    const graph = new Graph(grid);
+
+
+
+    const visited = new Array(graph.numRow).fill(false)
+        .map(() => new Array(graph.numCol).fill(false));
+
+
+    for (let i = 0; i < graph.numRow; i++) {
+        for (let j = 0; j < graph.numCol; j++) {
+
+            if (!visited[i][j] && graph.matrix[i][j] === 1) {
+                graph.dfs(i, j, visited);
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
 
 module.exports = countIslands;
--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,30 @@
-function wordSearch(words, word) {}
+function wordSearch(words, word) {
+    if (word === "") return true;
+
+    for (var row = 0; row < words.length; row++) {
+        for (var col = 0; col < words[row].length; col++) {
+            if (words[row][col] === word[0]) {
+                if (dfs(0, row, col)) return true;
+            }
+        }
+    }
+    return false;
+
+    function dfs(index, x, y) {
+        if (index === word.length) return true;
+        if (!words[x] || !words[x][y]) return false;
+        if (words[x][y] !== ' ' && words[x][y] === word[index]) {
+            var ch = words[x][y];
+            words[x][y] = ' ';
+
+            if (dfs(index + 1, x - 1, y)) return true; //up
+            if (dfs(index + 1, x + 1, y)) return true; //down
+            if (dfs(index + 1, x, y - 1)) return true; //left
+            if (dfs(index + 1, x, y + 1)) return true; //right
+            words[x][y] = ch; // backtracking
+        }
+        return false;
+    }
+}
 
 module.exports = wordSearch;
