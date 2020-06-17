3c3f2ae081fa531989f602911f2c1f15fda03d92
Hardey18 <35535871+Hardey18@users.noreply.github.com>
2020-06-16 19:51:41 -0700

    word search algorithm now working

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -4,14 +4,26 @@ function wordSearch(words, word) {
   for(let row = 0; row < words.length; row++) {
     for(let col = 0; col < words[row].length; col++) {
       if(words[row][col] === word[0]) {
-        if(dfs(0, row, col)) return true
+        if(search(0, row, col)) return true
       }
     }
   }
   return false
 
-  function dfs(index, x, y) {
-    
+  function search(index, x, y) {
+    if(index === word.length) return true
+    if(!words[x] || !words[x][y]) return false
+    if(words[x][y] !== "#" && words[x][y] === word[index]) {
+      let ch = words[x][y]
+      words[x][y] = "#"
+
+      if(search(index + 1, x - 1, y)) return true
+      if(search(index + 1, x + 1, y)) return true
+      if(search(index + 1, x, y - 1)) return true
+      if(search(index + 1, x, y + 1)) return true
+      words[x][y] = ch
+    }
+    return false
   }
 }
 
