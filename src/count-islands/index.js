c2d1c6bcc4585e83e19ff63536bb68af92f03b68
Hardey18 <35535871+Hardey18@users.noreply.github.com>
2020-06-16 23:51:43 -0700

    completed

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,13 +1,33 @@
 function countIslands(grid) {
+  let search = function(grid, row, col) {
+    if(grid[row] === undefined || grid[row][col] === undefined || grid[row][col] === 0) {
+      return 0
+    }
+
+    grid[row][col] = 0
+
+    search(grid, row - 1, col)
+    search(grid, row + 1, col)
+    search(grid, row, col - 1)
+    search(grid, row, col + 1)
+
+    return
+  }
+
+  if(grid.length === 0 || grid === null) {
+    return 0
+  }
+
   let count = 0
   for(let row = 0; row < grid.length; row++) {
     for(let col = 0; col < grid[row].length; col++) {
-      if(!visited[row][col] && grid[row][col] === "1") {
+      if(grid[row][col] === 1) {
         count++
-        markIsland(grid, row, col)
+        search(grid, row, col)
       }
     }
   }
+  return count
 }
 
 module.exports = countIslands;
