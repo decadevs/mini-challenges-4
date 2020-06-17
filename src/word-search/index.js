3b30e2352ff28f479648444c2b77687f75a9eaf7
Hardey18 <35535871+Hardey18@users.noreply.github.com>
2020-06-16 19:32:45 -0700

    looped through the array and points to another function

--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,18 @@
-function wordSearch(words, word) {}
+function wordSearch(words, word) {
+  if(word === "") return true
+
+  for(let row = 0; row < words.length; row++) {
+    for(let col = 0; col < words[row].length; col++) {
+      if(words[row][col] === word[0]) {
+        if(dfs(0, row, col)) return true
+      }
+    }
+  }
+  return false
+
+  function dfs(index, x, y) {
+    
+  }
+}
 
 module.exports = wordSearch;
