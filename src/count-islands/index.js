290b8f16c53f7a813c55388d6de52e9c46c1035c
Hardey18 <35535871+Hardey18@users.noreply.github.com>
2020-06-16 21:17:00 -0700

    looping through the array and calling the search function

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,13 @@
-function countIslands(grid) {}
+function countIslands(grid) {
+  let count = 0
+  for(let row = 0; row < grid.length; row++) {
+    for(let col = 0; col < grid[row].length; col++) {
+      if(!visited[row][col] && grid[row][col] === "1") {
+        count++
+        markIsland(grid, row, col)
+      }
+    }
+  }
+}
 
 module.exports = countIslands;
