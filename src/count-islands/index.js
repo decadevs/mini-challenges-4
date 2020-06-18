a2a99ad8f886f65881c7c0d12711610bcdc4a682
Mandong Lawrence <Mandonglawrence@gmail.com>
2020-06-18 17:33:19 +0100

    solve island and word search

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,32 @@
-function countIslands(grid) {}
+function countIslands(grid) {
+	let count = 0;
+	for(let i=0;i<grid.length;i++){
+		for(let j=0;j<grid[0].length;j++){
+			if(grid[i][j]==1){
+			dfs(grid,i,j);
+			count++;}
+		}
+	}
+	function dfs(grid,row,column){
+       // handle out of bound
+       if(row<0||column<0||row>grid.length-1||column>grid[0].length-1){
+       	return;
+       }
+       // handle water
+       if(grid[row][column]==0){
+       	return;
+       }
+       // process a cell containing 1 by converting it to zero
+       // in order to avoid processing that cell again
+       grid[row][column]=0;
+       
+       // recursive call to dfs
+       dfs(grid,row,column-1)//left
+       dfs(grid,row,column+1)//right
+       dfs(grid,row-1,column)//top
+       dfs(grid,row+1,column)//bottom
+	}
+	return count;
+}
 
 module.exports = countIslands;
--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,40 @@
-function wordSearch(words, word) {}
+function wordSearch(words,word) {
+        for(let i=0;i<words.length;i++){
+        	for(let j =0;j<words[0].length;j++){
+            if(words[i][j]==word.charAt(0) && dfs(words,i,j,word,0)){
+             return true;
+                }      		
+        	}
+        }
+        return false;
 
+function dfs(words,row,col,word,pointer){
+
+	if(pointer==word.length){
+   	return true;
+   }
+
+	// handle out of bound index
+	if(row<0 || col<0 || row>words.length-1 || 
+		col>words[0].length-1||
+		words[row][col]!=word.charAt(pointer)
+		){
+		return false;
+	}
+
+   // process
+   let temp =words[row][col];
+   words[row][col]="-"
+  let res =
+  dfs(words,row-1,col,word,pointer+1)||
+  dfs(words,row+1,col,word,pointer+1)||
+  dfs(words,row,col-1,word,pointer+1)||
+  dfs(words,row,col+1,word,pointer+1)
+ 
+  words[row][col]=temp;
+
+  return res;
+      
+  }
+}
 module.exports = wordSearch;
