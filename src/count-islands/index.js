bff94f11a68557ba60611605359f1d12dd141187
Dexkode <dexkode4@gmail.com>
2020-06-19 14:08:41 +0100

    week3 task completed

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -3,8 +3,7 @@ function countIslands(grid) {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
       if (grid[i][j] == 1) {
-        islandCount += 1;
-        dfs(grid, i, j);
+        islandCount += dfs(grid, i, j);
       }
     }
   }
@@ -12,22 +11,30 @@ function countIslands(grid) {
 }
 
 let dfs = (grid, i, j) => {
+  //boundary check
   if (
     i < 0 ||
     i >= grid.length ||
     j < 0 ||
     j >= grid[i].length ||
-    grid[i][j] == 0
+    grid[i][j] !== 1
   ) {
     return;
   }
-  grid[i][j] = 0;
+  grid[i][j] = -1;
   dfs(grid, i + 1, j);
   dfs(grid, i - 1, j);
   dfs(grid, i, j - 1);
   dfs(grid, i, j + 1);
+  return 1;
 };
-const grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+const grid = [
+  [1, 1, 1, 1, 0],
+  [1, 1, 0, 1, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+
 console.log(countIslands(grid));
 
 module.exports = countIslands;
