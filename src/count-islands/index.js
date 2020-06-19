53cac922cc096354cc4173dc5c16581772b725be
Iretii0511 <simonjulius0511@gmail.com>
2020-06-19 14:28:52 +0100

    Created a loop to access all nodes once

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,30 @@
-function countIslands(grid) {}
+function countIslands(grid) {
+    let numberOfIsland = 0;
+    const VISITED = [];
+    //
+    for(let i = 0; i < grid.length; ++i){
+        VISITED.push([]);
+    }
+    for(let column = 0; column < grid.length; ++column) {
+        for(let row = 0; row < grid[column].length; ++row)  {
+            if(grid[column][row] === 1 && !VISITED[column][row]) {
+                numberOfIsland++;
+                checkVisitedStatus(grid, column, row, VISITED)
+            }
+        }
+    }
+    return numberOfIsland;
+}
+
+function checkVisitedStatus (graph, yAxis, xAxis, visited){
+    if(yAxis < 0 || xAxis < 0 || yAxis > graph.length - 1 || xAxis > graph[yAxis].length - 1) return;
+    if(graph[yAxis][xAxis] === 0) return;
+    if(visited[yAxis][xAxis] === true) return;
+    visited[yAxis][xAxis] = true;
+    checkVisitedStatus(graph, yAxis - 1, xAxis, visited);
+    checkVisitedStatus(graph, yAxis + 1, xAxis, visited);
+    checkVisitedStatus(graph, yAxis, xAxis - 1, visited);
+    checkVisitedStatus(graph, yAxis, xAxis + 1, visited);
+}
 
 module.exports = countIslands;
--- src/word-search/index.js
+++ src/word-search/index.js
@@ -1,3 +1,17 @@
-function wordSearch(words, word) {}
+function wordSearch(words, word) {
+    for (let i = 0; i< words.length; ++i) {
+        for (let j = 0; j < words[i].length; ++j) {
+            if (words[i][j] === word[0]) {
 
+            }
+        }
+    }
+}
+
+function searchWords (graph, yAxis, xAxis, VISITED) {
+    if (yAxis < 0 || xAxis < 0 || yAxis > graph.length - 1 || xAxis > graph[yAxis] - 1) {
+        return false
+    };
+    
+}
 module.exports = wordSearch;
